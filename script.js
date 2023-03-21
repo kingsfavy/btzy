b83c45ebdd57880d0c7decb4bde52b065c138d2d
kingsfavy <122705122+kingsfavy@users.noreply.github.com>
2023-03-21 22:54:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+  
+  
+  function value(){
+    document.getElementById("demo"). innerHTML =  "hello people Ok serious youth, how far have you gone with your own project, me I've done my design using AutoCAD and someone else is helping me with my solids works, rn I'm almost done with with acquiring parts by next week  I should start fabrication";
+    document.getElementById("demo"). style. WebkitAnimation = "bottom 1s alternate-reverse ease-in-out 0s 1 forwards";
+    document.getElementById("demo"). style. marginLeft = "2%";
+ document.getElementById("demo"). style. color ="blue";
+ 
+         
+  }
+  
+  
+  function getValue(){
+  document.getElementById("elem"). innerHTML = "jire hello people Ok serious youth, how far have you gone with your own project, me I've done my design using AutoCAD and someone else is helping me with my solids works, rn I'm almost done with with acquiring parts by next week  I should start fabrication";
+    document.getElementById("elem"). style. WebkitAnimation = "bottom 1s alternate-reverse ease-in-out 0s 1 forwards";
+    document.getElementById("elem"). style. marginLeft = "2%";
+ document.getElementById("elem"). style. color ="black";
+  }
+  
+  function first(){
+    document.getElementById("first"). innerHTML = "Ok serious youth, how far have you gone with your own project, me I've done my design using AutoCAD and someone else is helping me with my solids works, rn I'm almost done with with acquiring parts by next week  I should start";
+    document.getElementById("first"). style. WebkitAnimation = "bottom 1s alternate-reverse ease-in-out 0s 1 forwards";
+    document.getElementById("first"). style. marginLeft = "2%";
+ document.getElementById("first"). style. color ="green";
+ 
+  }
+  
+  function sec(){
+    document.getElementById("second"). innerHTML = "hello people Ok serious youth, how far have you gone with your own project, me I've done my design using AutoCAD and someone else is helping me with my solids works, rn I'm almost done with with acquiring parts by next week  I should start fabrication";
+    document.getElementById("second"). style. WebkitAnimation = "bottom 1s alternate-reverse ease-in-out 0s 1 forwards";
+    document.getElementById("second"). style. marginLeft = "2%";
+  
+ document.getElementById("second"). style. color ="grey";
+  }
+  
+  function func(){
+  document.getElementById("body"). innerHTML = "accept" + " " + " " + " " + "decline";
+  }
+  
+  function myFunc(){
+  if(confirm("Was the Informations helpful?")) {
+         alert(" Thank you.").style .alert .borderRadius="10px";
+      } else {
+         alert("leave your comment in the comment box. Thank you.");
+      }
+  }
